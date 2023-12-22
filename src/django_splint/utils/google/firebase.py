1fd4ca2800ed2801930edb95b3eaf18e8664bef0
alexandrefialho <alexandrefialhobr@gmail.com>
2023-12-22 10:29:37 -0300

    Added class methods `create`, `update_or_create` and `get` to `FirebaseAuth` and `FirebaseDatabase` classes. These methods allow for easier creation, updating, and retrieval of user records in Firebase. This patch is necessary to provide more flexibility and convenience when working with Firebase user data.

--- src/django_splint/utils/google/firebase.py
+++ src/django_splint/utils/google/firebase.py
@@ -1,5 +1,9 @@
+from typing import Union
+
 from django.conf import settings
 from firebase_admin import auth, db
+from firebase_admin.auth import UserRecord
+from firebase_admin.db import Reference
 
 from ..api.authentication import clean_email
 from ..aws.function.logging import Logger
@@ -19,13 +23,27 @@ class FirebaseAuth:
             return cls.app.get_user(uid)
         return cls.app.get_user_by_email(email)
 
-    def create(self, email: str, payload: dict = {}, **kwargs) -> None:
+    @classmethod
+    def create(cls, email: str, payload: dict = {}, **kwargs) -> UserRecord:
+        """
+        Create firebase access for user
+
+        Args:
+            email (str): email of user
+            payload (dict, optional): payload to create user. Defaults to {}.
+
+        Raises:
+            error: EmailAlreadyExistsError
+
+        Returns:
+            UserRecord: UserRecord
+        """
         LOG.info(
             f"Creating firebase access for user {email}", extra={**kwargs, **payload}
         )
         kwargs.update({"email": email})
         try:
-            self.app.create_user(email=clean_email(email), **payload)
+            user = cls.app.create_user(email=clean_email(email), **payload)
         except auth.EmailAlreadyExistsError:
             LOG.warning(
                 f"User {email} already exists in firebase", extra={**kwargs, **payload}
@@ -34,15 +52,32 @@ class FirebaseAuth:
         LOG.info(
             f"Created firebase access for user {email}", extra={**kwargs, **payload}
         )
+        return user
 
-    def update_or_create(self, email: str, payload: dict, **kwargs) -> None:
+    @classmethod
+    def update_or_create(
+        cls, email: str, payload: dict, **kwargs
+    ) -> Union[UserRecord, bool]:
+        """
+        Update or create firebase access for user
+
+        Args:
+            email (str): email of user
+            payload (dict): payload to update
+
+        Raises:
+            error: EmailAlreadyExistsError
+
+        Returns:
+            Union[UserRecord, bool]: UserRecord and True if user was created, else False
+        """
         LOG.info(
             f"Updating firebase access of user {email}", extra={**kwargs, **payload}
         )
         try:
-            user = self.get(email)
+            user = cls.get(email)
         except auth.UserNotFoundError:
-            user = self.create(**payload)
+            user = cls.create(**payload)
             LOG.warning(
                 f"User {email} not found in firebase, creating new user...",
                 extra={**kwargs, **payload},
@@ -50,7 +85,7 @@ class FirebaseAuth:
             return user, True
 
         try:
-            user = self.app.update_user(user.uid, **payload)
+            user = cls.app.update_user(user.uid, **payload)
         except auth.EmailAlreadyExistsError as error:
             LOG.warning(
                 f"User {payload['email']} already exists in firebase",
@@ -64,13 +99,43 @@ class FirebaseAuth:
 
 
 class FirebaseDatabase:
+    @classmethod
+    def reference(
+        cls,
+        path,
+        uid=None,
+        url=settings.GOOGLE_FIREBASE_DATABASE_URL,
+    ) -> Reference:
+        """
+        Get firebase database reference
+
+        Args:
+            path (str): path to reference
+            uid (str, optional): uid of user. Defaults to None.
+            url (str, optional): url of firebase database. Defaults to settings.GOOGLE_FIREBASE_DATABASE_URL.
+
+        Returns:
+            Reference: firebase database reference
+        """
+        path = f"/{path}/{uid}/" if uid else f"/{path}/"
+        return db.reference(path=path, url=url)
 
     @classmethod
-    def get(self, path, uid, url=settings.GOOGLE_FIREBASE_DATABASE_URL):
-        return (
-            db
-            .reference(
-                path=f"/{path}/{uid}/",
-                url=url)
-            .get()
-        )
+    def get(
+        cls,
+        path,
+        uid,
+        url=settings.GOOGLE_FIREBASE_DATABASE_URL,
+    ) -> dict:
+        """
+        Get firebase database data
+
+        Args:
+            path (str): path to reference
+            uid (str): uid of user
+            url (str, optional): url of firebase database. Defaults to settings.GOOGLE_FIREBASE_DATABASE_URL.
+
+        Returns:
+            dict: firebase database data
+        """
+        return db.reference(path=f"/{path}/{uid}/", url=url).get()
