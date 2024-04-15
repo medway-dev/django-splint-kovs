3911a90c28f0d1066cd9dbc76366ec9b6fa777fa
Nayara Gerolomo <nayara.gerolomo@gmail.com>
2024-04-15 16:04:26 -0300

    style: better default params

--- src/django_splint/utils/aws/sqs.py
+++ src/django_splint/utils/aws/sqs.py
@@ -7,12 +7,17 @@ class AWSSQSHandler:
 
     service_name = 'sqs'
 
-    def __init__(self, aws_access_key_id=None, aws_secret_access_key=None, aws_region_name=None):
+    def __init__(
+            self,
+            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+            aws_region_name=settings.AWS_DEFAULT_REGION
+    ):
         self.client = boto3.client(
             service_name=self.service_name,
-            aws_access_key_id=aws_access_key_id or settings.AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=aws_secret_access_key or settings.AWS_SECRET_ACCESS_KEY,
-            region_name=aws_region_name or settings.AWS_DEFAULT_REGION,
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key,
+            region_name=aws_region_name,
         )
 
     def send_message(self, **kwargs):
