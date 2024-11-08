df7399d3298239248a92ce4e53f8213b05027b78
alexandrefialho <alexandrefialhobr@gmail.com>
2024-11-08 19:01:26 -0300

    rollback init

--- src/django_splint/db/models.py
+++ src/django_splint/db/models.py
@@ -84,10 +84,15 @@ class SplintModel(models.Model):
         original_value_fields = ()
         default_manager_name = 'objects'
 
-    def __setattr__(self, name: str, value: Any) -> None:
-        if name in self._meta.original_value_fields:
-            setattr(self, f"__original_{name}", value)
-        return super().__setattr__(name, value)
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        original_value_fields = getattr(self._meta, "original_value_fields", ())
+        list(
+            map(
+                lambda f: setattr(self, f"__original_{f}", getattr(self, f)),
+                original_value_fields,
+            )
+        )
 
     def save(self, log_activity=True, *args, **kwargs):
         """Save overwrite to log every every action in the system."""
